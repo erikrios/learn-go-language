5900250be58a5d3407b5cdf8096efe2f5264df31
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-19 11:55:04 +0700

    Create book_model.go

---
+++ my-library-go-fiber/app/models/book_model.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// Book struct to describe book object.
+type Book struct {
+	ID         uuid.UUID `db:"id" json:"id" validate:"required,uuid"`
+	CreatedAt  time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt  time.Time `db:"updated_at" json:"updated_at"`
+	UserId     uuid.UUID `db:"user_id" json:"user_id" validate:"required,uuid"`
+	Title      string    `db:"title" json:"title" validate:"required,lte=255"`
+	Author     string    `db:"author" json:"author" validate:"required,lte=255"`
+	BookStatus int       `db:"book_status" json:"book_status" validate:"required,len=1"`
+	BookAttrs  BookAttrs `db:"book_attrs" json:"book_attrs" validate:"required,dive"`
+}
+
+// BookAttrs struct to describe book attributes.
+type BookAttrs struct {
+	Picture     string `json:"picture"`
+	Description string `json:"description"`
+	Rating      string `json:"rating" validate:"min=1, max=10"`
+}
