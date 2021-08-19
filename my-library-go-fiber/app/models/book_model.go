c4cc8a25843f1438527eac09bac6facf787d2e5b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-19 12:06:09 +0700

    Create a method to parsing the data from json and to json

--- my-library-go-fiber/app/models/book_model.go
+++ my-library-go-fiber/app/models/book_model.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
 	"github.com/google/uuid"
 	"time"
 )
@@ -22,4 +25,21 @@ type BookAttrs struct {
 	Picture     string `json:"picture"`
 	Description string `json:"description"`
 	Rating      string `json:"rating" validate:"min=1, max=10"`
+}
+
+// Value make the BookAttrs struct implement the driver.Valuer interface.
+// This method simply returns the JSON-encoded representation of the struct.
+func (b BookAttrs) Value() (driver.Value, error) {
+	return json.Marshal(b)
+}
+
+// Scan make the BookAttrs struct implement the sql.Scanner interface.
+// This method simply decodes a JSON-encoded value into the struct fields.
+func (b *BookAttrs) Scan(value interface{}) error {
+	j, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(j, &b)
 }
