0e370df053e36e3d3640e90e03e57a627bff631c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-19 10:48:48 +0700

    Create 000001_create_init_tables.up.sql

--- my-library-go-fiber/platform/migrations/000001_create_init_tables.up.sql
+++ my-library-go-fiber/platform/migrations/000001_create_init_tables.up.sql
@@ -1,21 +1,24 @@
 -- Add UUID extension
-CREATE EXTENSION IF NOT EXISTS  "uuid-ossp";
+CREATE
+EXTENSION IF NOT EXISTS  "uuid-ossp";
 
 -- Set timezone
 SET
-TIMEZONE="Asia/Bangkok"
+TIMEZONE="Asia/Bangkok";
 
 -- Create books table
-CREATE TABLE books (
-    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
-    updated_at TIMESTAMP NULL,
-    title VARCHAR (255) NOT NULL,
-    author VARCHAR (255) NOT NULL,
-    book_status INT NOT NULL,
-    book_attrs JSONB NOT NULL
+CREATE TABLE books
+(
+    id          UUID                     DEFAULT uuid_generate_v4() PRIMARY KEY,
+    created_at  TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at  TIMESTAMP NULL,
+    title       VARCHAR(255) NOT NULL,
+    author      VARCHAR(255) NOT NULL,
+    book_status INT          NOT NULL,
+    book_attrs  JSONB        NOT NULL
 );
 
 
 -- Add indexes
-CREATE INDEX active_books ON books (title) WHERE book_status = 1;
+CREATE
+INDEX active_books ON books (title) WHERE book_status = 1;
