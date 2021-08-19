ce4e2144157df85df254d010c88c74d9ee718a6f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-19 22:39:21 +0700

    Implements the fiber app

--- my-library-go-fiber/main.go
+++ my-library-go-fiber/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"LearnGoLanguage/my-library-go-fiber/pkg/configs"
+	"LearnGoLanguage/my-library-go-fiber/pkg/middleware"
+	"LearnGoLanguage/my-library-go-fiber/pkg/routes"
+	utils2 "LearnGoLanguage/my-library-go-fiber/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+)
 
+// @title API
+// @version 1.0
+// @description This is an auto-generated API Docs.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email your@gmail.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @securityDefinitions.apiKey ApiKeyAuth
+// @in header
+// @name Authorization
+// @BasePath /api
+func main() {
+	// Define Fiber config.
+	config := configs.FiberConfig()
+
+	// Define a new Fiber app with config.
+	app := fiber.New(config)
+
+	// Middlewares.
+	middleware.FiberMiddleware(app) // Register Fiber's middleware for app.
+
+	// Routes.
+	routes.SwaggerRoute(app)  // Register a route for API Docs (Swagger).
+	routes.PublicRoutes(app)  // Register a public routes for app.
+	routes.PrivateRoutes(app) // Register a private routes for app.
+	routes.NotFoundRoute(app) // Register route for 404 Error.
+
+	// Start server (with graceful shutdown).
+	utils2.StartServerWithGracefulShutdown(app)
 }
